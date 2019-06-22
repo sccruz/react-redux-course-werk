ec25c2b5a43ddba0deff244dd54b338cf39e83d8
Shaun Cruz <shaun@uprisingtech.com>
2019-06-22 18:54:02 -0500

    =WIP

---
+++ src/components/App/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+// import unsplash from '../api/unsplash';
+import SearchBar from './SearchBar/SearchBar';
+// import ImageList from './ImageList';
+
+class App extends React.Component {
+  state = { images: [] };
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,4 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-const App = () => {
-  const buttonText = { text: 'Click me' };
-  const labelText = 'Enter name:';
-
-  return (
-    <div>
-      <label className="label" htmlFor="name">
-        {labelText}
-      </label>
-      <input id="name" type="text" />
-      <button style={{ backgroundColor: 'blue', color: 'white' }}>
-        {buttonText.text}
-      </button>
-    </div>
-  );
-};
-
 ReactDom.render(<App />, document.querySelector('#root'));
